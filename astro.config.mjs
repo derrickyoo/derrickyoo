4a668f55bacde3ed1b703a54324af47ffa6cbaec
Derrick Yoo <derrickyoo@gmail.com>
2024-01-21 09:07:10 -0700

    Update blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   adapter: netlify(),
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
